a043f5cd3897379abae674abbc0bce493a2de081
diagorn <mikhail.gasin@fors.ru>
2024-02-13 00:57:25 +0300

    initial blank project

---
+++ app/src/main/java/com/turing/android/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.turing.android
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Основное активити
+ *
+ * @author Diagorn
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
